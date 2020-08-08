a2832e638fc25a8bc1f6698f1886d6daebac5f76
mileallen <31962034+mileallen@users.noreply.github.com>
2020-08-08 16:34:56 +0530

    Even cleaner

--- code.js
+++ code.js
@@ -1,31 +1,38 @@
 
 const pageState = (function pageVars() {
-    const page = document.querySelector('#page')
+
+    const o = {
+        page: document.querySelector('#page'),
+        lbl: document.getElementById('slide').querySelector('div.pause'),
+        scts: document.querySelectorAll('div.sect'),
+        ppt: document.getElementById('slide').querySelector('img'),
+        men: document.getElementById('menubox')
+        }
+
     return function () {
-        return new Proxy({}, {
+        return new Proxy(o, {
             set: function (target, key, value) {
                 switch (key) {
                     case 'pause':
-                        let butText = document.getElementById('slide').querySelector('div.pause')
                         if (value) {
-                            clearInterval(pageState.trigSlides)
-                            clearTimeout(pageState.hideSlide)
-                            butText.innerText = "Resume"
+                            clearInterval(target.trigSlides)
+                            clearTimeout(target.hideSlide)
+                            target.lbl.innerText = "Resume"
                         }
                         else {
-                            pageState.trigSlides = setInterval(showSlides, 1700)
-                            butText.innerText = "Pause"
-                            butText.style.color = 'initial'
+                            target.trigSlides = setInterval(showSlides, 1700)
+                            target.lbl.innerText = "Pause"
+                            target.lbl.style.color = 'initial'
                         }
                         break
-                    case 'tab': case 'slid': page.dataset[key] = value
+                    case 'tab': case 'slid': target.page.dataset[key] = value
                         break
                 }
                 target[key] = value
                 return true
             },
             get: function (target, key) {
-                if (key === 'tab' || key === 'slid') return page.dataset[key]
+                if (key === 'tab' || key === 'slid') return target.page.dataset[key]
                 else return target[key]
             }
         })
@@ -34,15 +41,14 @@ const pageState = (function pageVars() {
 
 
 function menuState() {
-    let sections = document.querySelectorAll('div.sect')
-    let l = sections.length - 1
-    sections.forEach(el => {
+    let l = pageState.scts.length - 1
+    pageState.scts.forEach(el => {
         let pTop = el.getBoundingClientRect().top
         if (pTop > -5 && pTop < 50) {
             pageState.tab = el.getAttribute('indx')
         }
     })
-    if (sections[l].getBoundingClientRect().bottom < (screen.height + 20) && sections[l].getBoundingClientRect().bottom > (screen.height - 10)) {
+    if (pageState.scts[l].getBoundingClientRect().bottom < (screen.height + 20) && pageState.scts[l].getBoundingClientRect().bottom > (screen.height - 10)) {
         pageState.tab = l.toString()
     }
 }
@@ -52,26 +58,24 @@ pageState.trigSlides = setInterval(showSlides, 1700)
 
 
 function showSlides() {
-    let slideBox = document.getElementById('slide').querySelector('img')
     if (pageState.tab === "2") {
-        pageState.hideSlide = setTimeout(() => { slideBox.style.opacity = 0 }, 1400)
+        pageState.hideSlide = setTimeout(() => { pageState.ppt.style.opacity = 0 }, 1400)
 
         if (pageState.slid === "3") pageState.slid = "0"
         else pageState.slid++
 
-        slideBox.src = `img/slide-${pageState.slid}.png`
-        slideBox.style.opacity = 1
+        pageState.ppt.src = `img/slide-${pageState.slid}.png`
+        pageState.ppt.style.opacity = 1
     }
 }
 
 function menuTog() {
-    let men = document.getElementById('menubox')
-    let men_ht = parseFloat(men.style.height)
+    let men_ht = parseFloat(pageState.men.style.height)
     if (isNaN(men_ht) || men_ht === 0) {
-        men.style.height = '15rem'
+        pageState.men.style.height = '15rem'
     }
     else {
-        men.style.height = '0rem'
+        pageState.men.style.height = '0rem'
     }
 }
 
@@ -88,8 +92,7 @@ window.addEventListener('load', function (event) {
 }, false)
 
 window.addEventListener('resize', function (event) {
-    let men = document.getElementById('menubox')
     if (screen.width > 350) {
-        men.style.height = '15rem'
+        pageState.men.style.height = '15rem'
     }
 }, false)
