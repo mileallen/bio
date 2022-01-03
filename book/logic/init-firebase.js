3ca86e66554cde9fab9bfd48154f447297afff9f
mileallen <31962034+mileallen@users.noreply.github.com>
2022-01-03 12:13:34 +0530

    added new sub site
    moved book site away from angle.fyi

---
+++ book/logic/init-firebase.js
@@ -0,0 +1,13 @@
+
+  // The app's configuration
+  var fbConfig = {
+    apiKey: "AIzaSyB9nLRom9gnvFCKdyQJ54rJvo5CbFIO8Fk",
+    authDomain: "alert-list.firebaseapp.com",
+    databaseURL: "https://alert-list.firebaseio.com",
+    projectId: "alert-list",
+    storageBucket: "alert-list.appspot.com",
+    messagingSenderId: "260917195312",
+    appId: "1:260917195312:web:8287ca965c4327fb8cb3f5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(fbConfig);
