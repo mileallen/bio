25748883a75ae892a35f3eeb536b8f7ccd64fa56
mileallen <31962034+mileallen@users.noreply.github.com>
2023-01-04 11:09:21 +0530

    code and date
    It's 2023! © Switch-Case is so... 2003?

--- code.js
+++ code.js
@@ -3,85 +3,40 @@ const pageState = (function () {
 
     const o = {
         _page: document.querySelector('#page'),
-        _lbl: document.querySelector('div.pause'),
         _scts: document.querySelectorAll('div.sect'),
         _ppt: document.querySelector('#slide').querySelector('img'),
         _men: document.querySelector('#menubox'),
-        slid: -1,
+        _slid: -1,
+        set_pause: function (v) { return true // do nothing
+        },
+        set_slid: function (v){
+                        this._ppt.classList.add("transit")                        
+                        setTimeout(() => {
+                            this._ppt.src = `img/slide-${v}.png`
+                            this._ppt.classList.remove("transit")
+                        }, 350)
+        }
     }
-
         return new Proxy(o, {
             set(t, k, v) {
-                switch (k) {
-                    case 'pause':
-                        if (v) {
-                            clearInterval(t._trigSlides)
-                            clearTimeout(t._hideSlide)
-                            t._lbl.innerText = "Resume"
-                        }
-                        else {
-                            t._trigSlides = setInterval(showSlides, 1700)
-                            t._lbl.innerText = "Pause"
-                            t._lbl.style.color = 'initial'
-                        }
-                        break
-                    case 'tab': t._page.dataset.tab = v
-                        break                    
-                    case 'slid':
-                        t._ppt.classList.add("transit")                        
-                        setTimeout(() => {
-                            t._ppt.src = `img/slide-${v}.png`
-                            t._ppt.classList.remove("transit")
-                        }, 350)
-                        t._page.dataset.slid = v
-                        break
-                }
+                if ( `set_${k}` in t ) t[`set_${k}`](v)
+                t._page.dataset[k] = v
                 t[`_${k}`] = v
                 return true
             },
-            get(t, k) {
-                if (k === 'tab' || k === 'slid') return t._page.dataset[k]
-                else return t[`_${k}`]
-            }
+            get(t, k) { return t[`_${k}`] }
         })
 })()
 
-
 function menuState() {
-    let l = pageState.scts.length - 1
-    pageState.scts.forEach(el => {
-        let pTop = el.getBoundingClientRect().top
-        if (pTop > -5 && pTop < 50) {
-            pageState.tab = el.getAttribute('indx')
-        }
-    })
-    if (pageState.scts[l].getBoundingClientRect().bottom < (screen.height + 20) && pageState.scts[l].getBoundingClientRect().bottom > (screen.height - 10)) {
-        pageState.tab = l.toString()
-    }
+    let l = document.querySelector('.works'), h = window.innerHeight, s
+    s = document.elementFromPoint( l.offsetLeft + 10, Math.floor(h/3) )
+    pageState.tab = s.getAttribute('indx')
 }
-
-/*
-
-pageState.trigSlides = setInterval(showSlides, 1700)
-
-
-function showSlides() {
-        pageState.hideSlide = setTimeout(() => { pageState.ppt.style.opacity = 0 }, 1400)
-
-        if (pageState.slid == "3") pageState.slid = "0"
-        else pageState.slid++
-}
-*/
-
 
 function menuTog() {
     let men_ht = parseFloat(pageState.men.style.height)
-    if (isNaN(men_ht) || men_ht === 0) {
-        pageState.men.style.height = '15rem'
-    }
-    else {
-        pageState.men.style.height = '0rem'
-    }
+    pageState.men.style.height = !men_ht ? '15rem' : '0rem'
 }
 
 function fetchImgs() {
@@ -93,11 +48,6 @@ function fetchImgs() {
         late2[i] = new Image()
         late2[i].src = `img/slide-${i}.png`
     }
-}
-
-function setslide(j){
-    //pageState.pause = true
-    pageState.slid = j
 }
 
 
